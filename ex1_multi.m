7402551908b46c2aa63bb6d9814c982f77236cf4
calvinrs <stewarca@STEWARCA-7W.analytics.moodys.net>
2016-02-26 17:02:36 +0000

    optional excercises complete

--- ex1_multi.m
+++ ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.03; % changed from 0.01 (note that the cost is higher..)
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -104,7 +104,11 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+
+N = [1650, 3];
+NNorm = (N - mu) ./ sigma;
+Nfinal = [1, NNorm];
+price = Nfinal * theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +153,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3]*theta; % You should change this
 
 
 % ============================================================
